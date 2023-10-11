85f6b6c74bad1868c400ebdcceb7e409caeaea98
ernydavid <104800780+ernydavid@users.noreply.github.com>
2023-10-11 13:25:47 -0500

    improvement json for coupons and promos

---
+++ assets/ErrorPage-71a4bc9d.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-1b99a67c.js";function s(){return e.jsxs("div",{className:"w-full h-[90vh] flex flex-col gap-3 justify-center items-center",children:[e.jsx("h1",{className:"text-3xl",children:"Oops! There is an error"}),e.jsx("p",{children:"Please, go back or verify the url"})]})}export{s as default};
---
+++ assets/LoyaltySystemPage-79ff3b00.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-1b99a67c.js";function t(){return e.jsx("div",{className:"px-6 max-w-[1000px] m-auto",children:e.jsx("section",{className:"h-[90vh] flex flex-col justify-center items-center",children:e.jsx("h1",{children:"Hello World"})})})}export{t as default};
