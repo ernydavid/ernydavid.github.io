714579d7f1689a5e69780df8e80224ea17d2fcb8
ernydavid <104800780+ernydavid@users.noreply.github.com>
2023-10-11 12:39:06 -0500

    improvement Features

---
+++ assets/ErrorPage-3839435f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-09d7202d.js";function s(){return e.jsxs("div",{className:"w-full h-[90vh] flex flex-col gap-3 justify-center items-center",children:[e.jsx("h1",{className:"text-3xl",children:"Oops! There is an error"}),e.jsx("p",{children:"Please, go back or verify the url"})]})}export{s as default};
